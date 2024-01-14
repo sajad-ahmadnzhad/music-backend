77fa02315907bbf590c1f8f70e5ee8df6fc7963c
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-01-14 14:48:39 +0330

    featuer: Add featuer in category model

--- models/category.ts
+++ models/category.ts
@@ -3,7 +3,7 @@ import { Schema, model } from "mongoose";
 const schema = new Schema(
   {
     title: { type: String, required: true },
-    parent_id: { type: Schema.ObjectId },
+    parent_id: { type: Schema.ObjectId , ref: 'categoryParent' },
     description: { type: String },
     image: { type: String },
   },

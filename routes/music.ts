dee8017e4577fd1ab2781730780bc9df7701d4c5
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-01-14 14:22:10 +0330

    feat: Using music uploader in music router

--- routes/music.ts
+++ routes/music.ts
@@ -1,10 +1,19 @@
-import express from 'express';
-import { create } from '../controllers/music';
-import authMiddlewares from '../middlewares/auth';
-import isAdminMiddlewares from '../middlewares/isAdmin';
-import validatorMiddlewares from '../middlewares/validator';
-import musicValidator from './../validators/music'
-const router = express.Router()
-router.route('/').post(authMiddlewares , isAdminMiddlewares ,validatorMiddlewares(musicValidator), create)
+import express from "express";
+import { create } from "../controllers/music";
+import authMiddlewares from "../middlewares/auth";
+import isAdminMiddlewares from "../middlewares/isAdmin";
+import validatorMiddlewares from "../middlewares/validator";
+import musicValidator from "./../validators/music";
+import musicUploader from "./../utils/uploader/music";
+const router = express.Router();
+router
+  .route("/")
+  .post(
+    authMiddlewares,
+      isAdminMiddlewares,
+      musicUploader.single('music'),
+    validatorMiddlewares(musicValidator),
+    create
+  );
 
-export default router
+export default router;

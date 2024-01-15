18f5f8d6d3a19bf24172de8c07eb44988815ddd1
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-01-15 18:32:21 +0330

    feature: add regex in validator music duration

--- validators/music.ts
+++ validators/music.ts
@@ -4,7 +4,13 @@ export default joi.object({
   title: joi.string().max(100).min(5).required(),
   artist: joi.string().max(25).min(3).required(),
   genre: joi.string().max(30).max(3).required(),
-  duration: joi.string().max(6).required(),
+  duration: joi
+    .string()
+    .regex(/^\d{2}:\d{2}$/)
+    .message(
+      "This type of time is wrong. Enter a correct time, for example: 02:20 or 3:09"
+    )
+    .required(),
   release_year: joi
     .number()
     .integer()

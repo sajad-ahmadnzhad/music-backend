ae8f10537d035c577153da46156f9a7fe6837e64
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-01-16 18:28:55 +0330

    feat: create validator category and category parent

---
+++ validators/category.ts
@@ -0,0 +1,8 @@
+import joi from 'joi';
+const schema = joi.object({
+    title: joi.string().min(3).max(40).required(),
+    categoryParent: joi.string().required(),
+    description: joi.string().min(10).max(300)
+})
+
+export default schema
---
+++ validators/categoryParent.ts
@@ -0,0 +1,7 @@
+import joi from "joi";
+const schema = joi.object({
+  title: joi.string().min(3).max(40).required(),
+  description: joi.string().min(10).max(300),
+});
+
+export default schema;

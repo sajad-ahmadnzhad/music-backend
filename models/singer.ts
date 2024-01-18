2a60991a73ab77aed5abdeec9699061f5960f35e
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-01-18 21:59:53 +0330

    feature: add new fields in singer model

--- models/singer.ts
+++ models/singer.ts
@@ -2,10 +2,13 @@ import { Schema, model } from "mongoose";
 
 const schema = new Schema({
   fullName: { type: String, required: true },
-  photo: { type: String, required: true },
-  nickname: { type: String },
   englishName: { type: String, required: true },
-  musicStyle: { type: Schema.ObjectId, ref: 'categories', required: true },
+  nickname: { type: String },
+  photo: { type: String, required: true },
+  nationality: { type: String, required: true },
+  countViews: { type: Number, default: 0 },
+  countLike: { type: Number, default: 0 },
+  musicStyle: { type: Schema.ObjectId, ref: "categories", required: true },
+  albums: { type: [Schema.ObjectId], ref: "albums", required: true },
 });
-
 export default model("singer", schema);

8b9d0f296096e1135f15b982eee5503f3ecf30e8
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-01-19 00:05:54 +0330

    feat: create playList model

---
+++ models/playList.ts
@@ -0,0 +1,15 @@
+import { Schema, model } from "mongoose";
+
+const schema = new Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String },
+    user: { type: Schema.ObjectId, ref: "users", required: true },
+    musics: { type: [Schema.ObjectId], ref: "music" , default: [] },
+    albums: { type: [Schema.ObjectId], ref: "album" ,default: []},
+    category: { type: Schema.ObjectId, ref: "categories" },
+  },
+  { timestamps: true }
+);
+
+export default model("playList", schema);

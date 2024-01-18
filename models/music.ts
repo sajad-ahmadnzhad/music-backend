90aa333731c1be856b7ead0b3a4e6854fa0f07cd
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-01-18 22:16:42 +0330

    feature: added new fiedls in music model

--- models/music.ts
+++ models/music.ts
@@ -4,14 +4,18 @@ const schema = new Schema(
   {
     title: { type: String, required: true },
     artist: { type: String, required: true },
-    genre: { type: String, required: true },
     duration: { type: String, required: true },
     release_year: { type: Number, required: true },
     cover_image: { type: String, required: true },
-    music: { type: String, required: true },
+    download_link: { type: String, required: true },
     description: { type: String },
     rating: { type: Number, default: 3 },
-    createBy: {type: Schema.ObjectId , ref: 'users', required: true}
+    count_views: { type: Number, default: 0 },
+    count_likes: { type: Number, default: 0 },
+    count_downloads: { type: Number, default: 0 },
+    genre: { type: Schema.ObjectId, ref: "categories", required: true },
+    createBy: { type: Schema.ObjectId, ref: "users", required: true },
+    albums: { type: Schema.ObjectId, ref: "albums" },
   },
   { timestamps: true }
 );

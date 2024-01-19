60093b829daa5944c4b3210e1c7d15dfa7c138c1
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-01-19 18:20:53 +0330

    feature: add field nationaliy in singer validator

--- validators/singer.ts
+++ validators/singer.ts
@@ -5,4 +5,5 @@ export default Joi.object({
   nickname: Joi.string().trim().min(5).max(30),
   englishName: Joi.string().trim().min(5).max(30).required(),
   musicStyle: Joi.string().required(),
+  nationality: Joi.string().min(3).max(15).required(),
 });

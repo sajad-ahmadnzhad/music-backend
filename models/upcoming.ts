21f3fd6b7337c8fc51efa2ea8e67e2f3957ba730
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-01-19 14:18:45 +0330

    feat: create model upcoming

---
+++ models/upcoming.ts
@@ -0,0 +1,15 @@
+import { Schema, model } from "mongoose";
+
+const schema = new Schema(
+  {
+    title: { type: String, required: true },
+    artist: { type: Schema.ObjectId, ref: "singer", required: true },
+    release_date: { type: String },
+    genre: { type: Schema.ObjectId, ref: "categories", required: true },
+    description: { type: String },
+    cover_image: { type: String },
+  },
+  { timestamps: true }
+);
+
+export default model("upcoming", schema);

e2cc96880bd842bf78e24796369dca89e67ce9ca
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-01-19 14:41:21 +0330

    feat: create criticism model

---
+++ models/criticism.ts
@@ -0,0 +1,12 @@
+import { Schema, model } from "mongoose";
+
+const schema = new Schema({
+  user: { type: Schema.ObjectId, ref: "users", required: true },
+  type: { type: String, enum: ["music", "singer"], required: true },
+  body: { type: String, required: true },
+  score: { type: Number, enum: [1, 2, 3, 4, 5], default: 5 },
+  target_id: { type: Schema.ObjectId, refPath: "type", required: true },
+  isAccept: { type: Boolean, default: false },
+});
+
+export default model("criticism", schema);

ba89e606658dbd7b55c6664b493ccf1a96e92f06
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-01-19 17:27:21 +0330

    feature: add timestamps in users model

--- models/users.ts
+++ models/users.ts
@@ -1,12 +1,15 @@
 import { Schema, model } from "mongoose";
 
-const schema = new Schema({
-  name: { type: String, required: true, trim: true },
-  username: { type: String, require: true, trim: true  , unique: true},
-  email: { type: String, required: true , unique: true },
-  password: { type: String, required: true, trim: true },
-  isAdmin: { type: Boolean, default: false },
-  isSuperAdmin: { type: Boolean, default: false },
-  profile: {type:String , required: true}
-});
-export default model('users' , schema)
+const schema = new Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    username: { type: String, require: true, trim: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, trim: true },
+    isAdmin: { type: Boolean, default: false },
+    isSuperAdmin: { type: Boolean, default: false },
+    profile: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+export default model("users", schema);

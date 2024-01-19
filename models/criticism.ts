1916c4904420deaf4b32cd0cfd06c610cf5f8cb3
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-01-19 14:42:22 +0330

    feature: edit criticism model

--- models/criticism.ts
+++ models/criticism.ts
@@ -1,12 +1,15 @@
 import { Schema, model } from "mongoose";
 
-const schema = new Schema({
-  user: { type: Schema.ObjectId, ref: "users", required: true },
-  type: { type: String, enum: ["music", "singer"], required: true },
-  body: { type: String, required: true },
-  score: { type: Number, enum: [1, 2, 3, 4, 5], default: 5 },
-  target_id: { type: Schema.ObjectId, refPath: "type", required: true },
-  isAccept: { type: Boolean, default: false },
-});
+const schema = new Schema(
+  {
+    user: { type: Schema.ObjectId, ref: "users", required: true },
+    type: { type: String, enum: ["music", "singer"], required: true },
+    body: { type: String, required: true },
+    score: { type: Number, enum: [1, 2, 3, 4, 5], default: 5 },
+    target_id: { type: Schema.ObjectId, refPath: "type", required: true },
+    isAccept: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
 
 export default model("criticism", schema);

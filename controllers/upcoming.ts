a42b3e061d4278506d6960a2e0f0a5899b3269d5
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-01-25 23:33:08 +0330

    feat: create api remove upcoming

--- controllers/upcoming.ts
+++ controllers/upcoming.ts
@@ -4,6 +4,7 @@ import upcomingModel from "../models/upcoming";
 import httpStatus from "http-status";
 import fs from "fs";
 import path from "path";
+import { isValidObjectId } from "mongoose";
 export let create = async (req: express.Request, res: express.Response) => {
   const { title, artist, genre, description, release_date } =
     req.body as upcomingBody;
@@ -29,6 +30,7 @@ export let create = async (req: express.Request, res: express.Response) => {
     description,
     release_date,
     cover_image: coverName && `/upcomingCovers/${coverName}`,
+    createBy: (req as any).user._id,
   });
 
   res
@@ -38,9 +40,44 @@ export let create = async (req: express.Request, res: express.Response) => {
 export let getAll = async (req: express.Request, res: express.Response) => {
   const allUpcoming = await upcomingModel
     .find()
-    .populate("artist", 'fullName photo')
-    .populate("genre" , 'title description')
+    .populate("artist", "fullName photo")
+    .populate("genre", "title description")
     .lean();
   allUpcoming.sort((a: any, b: any) => b.createdAt - a.createdAt);
   res.json(allUpcoming);
+};
+export let remove = async (req: express.Request, res: express.Response) => {
+  const { id } = req.params;
+  const { user } = req as any;
+
+  if (!isValidObjectId(id)) {
+    res
+      .status(httpStatus.BAD_REQUEST)
+      .json({ message: "upcoming id is not from mongodb" });
+    return;
+  }
+
+  const upcoming = await upcomingModel.findById(id);
+
+  if (!upcoming) {
+    res.status(httpStatus.BAD_REQUEST).json({ message: "Upcoming not found" });
+    return;
+  }
+
+  if (user._id !== upcoming.createBy && !user.isSuperAdmin) {
+    if (upcoming.cover_image) {
+      fs.unlinkSync(path.join(process.cwd(), "public", upcoming.cover_image));
+    }
+    res.status(httpStatus.BAD_REQUEST).json({
+      message: "This album can only be modified by the person who created it",
+    });
+    return;
+  }
+
+  if (upcoming.cover_image) {
+    fs.unlinkSync(path.join(process.cwd(), "public", upcoming.cover_image));
+  }
+  await upcomingModel.findByIdAndDelete(id);
+
+  res.json({ message: "Deleted upcoming successfully" });
 };
--- routes/upcoming.ts
+++ routes/upcoming.ts
@@ -2,7 +2,7 @@ import express from "express";
 import authMiddlewares from "../middlewares/auth";
 import isAdminMiddlewares from "../middlewares/isAdmin";
 import validatorMiddlewares from "../middlewares/validator";
-import { create, getAll } from "../controllers/upcoming";
+import { create, getAll, remove } from "../controllers/upcoming";
 import imageUploader from "../utils/uploader/profile";
 import upcomingValidator from "../validators/upcoming";
 const router = express.Router();
@@ -15,6 +15,9 @@ router
     imageUploader.single("upcomingCover"),
     validatorMiddlewares(upcomingValidator),
     create
-  ).get(getAll);
+  )
+  .get(getAll);
+
+router.route("/:id").delete(authMiddlewares, isAdminMiddlewares, remove);
 
 export default router;

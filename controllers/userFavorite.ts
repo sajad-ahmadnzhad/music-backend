f0571cb3f0b23e2f3af385101b33a7f957facd6f
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-01-26 15:52:16 +0330

    feature: Add hard validation

--- controllers/userFavorite.ts
+++ controllers/userFavorite.ts
@@ -1,13 +1,40 @@
 import express from "express";
 import { UserFavoriteBody } from "../interfaces/userFavorite";
 import userFavoriteModel from "../models/userFavorite";
+import albumModel from "../models/album";
+import musicModel from "../models/music";
 import httpStatus from "http-status";
 export let create = async (req: express.Request, res: express.Response) => {
   const { type, target_id } = req.body as UserFavoriteBody;
-    const { user } = req as any;
-    
-    await userFavoriteModel.create({ type, target_id, user: user._id });
-    
+  const { user } = req as any;
+
+  const album = await albumModel.findById(target_id);
+  const music = await musicModel.findById(target_id);
+
+  if (type === "music" && !music) {
+    res.status(httpStatus.NOT_FOUND).json({ message: "Music not found" });
+    return;
+  }
+
+  if (type === "album" && !album) {
+    res.status(httpStatus.NOT_FOUND).json({ message: "Album not found" });
+    return;
+  }
+
+  const userFavorite = await userFavoriteModel.findOne({
+    user: user._id,
+    target_id,
+  });
+
+  if (userFavorite) {
+    res.status(httpStatus.BAD_REQUEST).json({
+      message: "This album or music is already in the favorite list",
+    });
+    return;
+  }
+
+  await userFavoriteModel.create({ type, target_id, user: user._id });
+
   res
     .status(httpStatus.CREATED)
     .json({ message: "Create user favorite successfully" });

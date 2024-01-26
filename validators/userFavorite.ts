2c33ebc2d3c87702c52bc40f7b5728238345ac22
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-01-26 15:41:58 +0330

    edit: edit user favorite validator

--- validators/userFavorite.ts
+++ validators/userFavorite.ts
@@ -1,5 +1,4 @@
 import joi from "joi";
-import userFavoriteModel from "../models/userFavorite";
 export default joi.object({
   type: joi.string().valid("music", "album").required(),
   target_id: joi
@@ -7,11 +6,4 @@ export default joi.object({
     .regex(/^[0-9a-fA-F]{24}$/)
     .message("target id is not from mongodb")
     .required()
-    .external(async (value, helpers) => {
-      const userFavorite = await userFavoriteModel.findById(value);
-      if (userFavorite)
-        return helpers.error(
-          "This music or album already exists in the database"
-        );
-    }),
 });

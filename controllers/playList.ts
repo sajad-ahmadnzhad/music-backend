fdc9ae13b0c4c9fab264dbfd38114eaea7486ec8
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-01-27 17:44:43 +0330

    refactor: refactor api create play list

--- controllers/playList.ts
+++ controllers/playList.ts
@@ -1,44 +1,39 @@
-import express from "express";
+import express,{Request , Response , NextFunction} from "express";
 import { PlayListBody } from "../interfaces/playList";
 import httpStatus from "http-status";
 import fs from "fs";
 import path from "path";
 import playListModel from "../models/playList";
 import { isValidObjectId } from "mongoose";
+import httpErrors from 'http-errors'
 
-export let create = async (req: express.Request, res: express.Response) => {
-  const cover = req.file?.filename;
-  try {
-    const body = req.body as PlayListBody;
-    const { user } = req as any;
-    if (!cover) {
-      throw new Error("cover play list is required");
+export let create = async (req: Request, res: Response, next: NextFunction) => {
+    const cover = req.file?.filename;
+    try {
+        const body = req.body as PlayListBody;
+        const { user } = req as any;
+        if (!cover) {
+            throw httpErrors.BadRequest("cover play list is required");
+        }
+
+        const playList = await playListModel.findOne({ title: body.title });
+
+        if (playList) {
+            throw httpErrors.BadRequest("This play list already exists");
+        }
+
+        await playListModel.create({
+            ...body,
+            createBy: user._id,
+            cover_image: `/playListCovers/${cover}`,
+        });
+
+        res
+            .status(httpStatus.CREATED)
+            .json({ message: "Create play list successfully" });
+    } catch (error: any) {
+        next(error)
     }
-
-    const playList = await playListModel.findOne({ title: body.title });
-
-    if (playList) {
-      throw new Error("This play list already exists");
-    }
-
-    await playListModel.create({
-      ...body,
-      createBy: user._id,
-      cover_image: `/playListCovers/${cover}`,
-    });
-
-    res
-      .status(httpStatus.CREATED)
-      .json({ message: "Create play list successfully" });
-  } catch (error: any) {
-    cover &&
-      fs.unlinkSync(
-        path.join(process.cwd(), "public", "playListCovers", cover)
-      );
-    res
-      .status(error.status || httpStatus.INTERNAL_SERVER_ERROR)
-      .json({ message: error.message || "Internal Server Error !!" });
-  }
 };
 export let getAll = async (req: express.Request, res: express.Response) => {
   try {

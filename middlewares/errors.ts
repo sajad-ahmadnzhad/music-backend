2037272fa2eb2f9686751239a882daaec30e9b1d
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-01-27 17:38:08 +0330

    feature: add feature remove file to error middleware

--- middlewares/errors.ts
+++ middlewares/errors.ts
@@ -1,8 +1,36 @@
-import express from 'express';
-export default (err:any , req:express.Request , res:express.Response , next:express.NextFunction) => {
-    if (err) {
-        const statusCode = err.status || err.statusCode || err.status || 500
-        const errorMessage = err.message || "Server Error !!"
-        res.status(statusCode).json({errorMessage})
+import express from "express";
+import fs from "fs";
+import path from "path";
+export default (
+  err: any,
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  if (err) {
+    try {
+      if (req.file) {
+        const folders = fs.readdirSync(path.join(process.cwd(), "public"));
+
+        folders.forEach((folder) => {
+          const files = fs.readdirSync(
+            path.join(process.cwd(), "public", folder)
+          );
+
+          if (files.includes(req.file!.filename)) {
+            fs.unlinkSync(
+              path.join(process.cwd(), "public", folder, req.file!.filename)
+            );
+          }
+        });
+      }
+      const statusCode = err.status || err.statusCode || 500;
+      const message = err.message || "Internal Server Error !!";
+      res.status(statusCode).json({ message });
+    } catch (error: any) {
+      res
+        .status(error.status || 500)
+        .json({ message: error.message || "Internal Server Error !!" });
     }
-}
+  }
+};

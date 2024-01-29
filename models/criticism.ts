c107dec994bd7c8b0022f9464bfaef9fddb03652
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-01-29 14:39:23 +0330

    feat: add new fields in criticism model

--- models/criticism.ts
+++ models/criticism.ts
@@ -2,9 +2,12 @@ import { Schema, model } from "mongoose";
 
 const schema = new Schema(
   {
-    user: { type: Schema.ObjectId, ref: "users", required: true },
+    creator: { type: Schema.ObjectId, ref: "users", required: true },
     type: { type: String, enum: ["music", "singer"], required: true },
     body: { type: String, required: true },
+    count_likes: { type: Number, default: 0 },
+    count_dislikes: { type: Number, default: 0 },
+    mainCriticismId: { type: Schema.ObjectId, ref: 'criticism' },
     score: { type: Number, enum: [1, 2, 3, 4, 5], default: 5 },
     target_id: { type: Schema.ObjectId, refPath: "type", required: true },
     isAccept: { type: Boolean, default: false },

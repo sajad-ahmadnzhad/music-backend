d062da66c5a01fffad40bd274dda5de62b6f3278
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-02-05 22:46:55 +0330

    refactor: all apis upcoming

--- controllers/upcoming.ts
+++ controllers/upcoming.ts
@@ -1,180 +1,156 @@
-import express from "express";
+import { Request, Response, NextFunction } from "express";
 import { upcomingBody } from "../interfaces/upcoming";
 import upcomingModel from "../models/upcoming";
 import httpStatus from "http-status";
+import httpErrors from "http-errors";
 import fs from "fs";
 import path from "path";
 import { isValidObjectId } from "mongoose";
-export let create = async (req: express.Request, res: express.Response) => {
-  const { title, artist, genre, description, release_date } =
-    req.body as upcomingBody;
-  const coverName = req.file?.filename;
+export let create = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const body = req.body as upcomingBody;
+    const coverName = req.file?.filename;
 
-  const upcomingMusic = await upcomingModel.findOne({ title, artist });
+    const upcomingMusic = await upcomingModel.findOne({
+      title: body.title,
+      artist: body.artist,
+    });
 
-  if (upcomingMusic) {
-    if (coverName) {
-      fs.unlinkSync(
-        path.join(process.cwd(), "public", "upcomingCovers", coverName)
+    if (upcomingMusic) {
+      throw httpErrors.Conflict("This upcoming music already exists");
+    }
+
+    await upcomingModel.create({
+      ...body,
+      cover_image: coverName && `/upcomingCovers/${coverName}`,
+      createBy: (req as any).user._id,
+    });
+
+    res
+      .status(httpStatus.CREATED)
+      .json({ message: "Create new upcoming successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+export let getAll = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const allUpcoming = await upcomingModel
+      .find()
+      .populate("artist", "fullName photo")
+      .populate("genre", "title description")
+      .populate("createBy", "name username profile")
+      .lean();
+    allUpcoming.sort((a: any, b: any) => b.createdAt - a.createdAt);
+    res.json(allUpcoming);
+  } catch (error) {
+    next(error);
+  }
+};
+export let remove = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { id } = req.params;
+    const { user } = req as any;
+
+    if (!isValidObjectId(id)) {
+      throw httpErrors.BadRequest("upcoming id is not from mongodb");
+    }
+
+    const upcoming = await upcomingModel.findById(id);
+
+    if (!upcoming) {
+      throw httpErrors.NotFound("upcoming not found");
+    }
+
+    if (user._id !== upcoming.createBy && !user.isSuperAdmin) {
+      throw httpErrors.Forbidden(
+        "This upcoming can only be remove by the person who created it"
       );
     }
-    return res
-      .status(httpStatus.BAD_REQUEST)
-      .json({ message: "This upcoming music already exists" });
-  }
 
-  await upcomingModel.create({
-    title,
-    artist,
-    genre,
-    description,
-    release_date,
-    cover_image: coverName && `/upcomingCovers/${coverName}`,
-    createBy: (req as any).user._id,
-  });
-
-  res
-    .status(httpStatus.CREATED)
-    .json({ message: "Create new upcoming successfully" });
-};
-export let getAll = async (req: express.Request, res: express.Response) => {
-  const allUpcoming = await upcomingModel
-    .find()
-    .populate("artist", "fullName photo")
-    .populate("genre", "title description")
-    .populate("createBy", "name username profile")
-    .lean();
-  allUpcoming.sort((a: any, b: any) => b.createdAt - a.createdAt);
-  res.json(allUpcoming);
-};
-export let remove = async (req: express.Request, res: express.Response) => {
-  const { id } = req.params;
-  const { user } = req as any;
-
-  if (!isValidObjectId(id)) {
-    res
-      .status(httpStatus.BAD_REQUEST)
-      .json({ message: "upcoming id is not from mongodb" });
-    return;
-  }
-
-  const upcoming = await upcomingModel.findById(id);
-
-  if (!upcoming) {
-    res.status(httpStatus.BAD_REQUEST).json({ message: "Upcoming not found" });
-    return;
-  }
-
-  if (user._id !== upcoming.createBy && !user.isSuperAdmin) {
     if (upcoming.cover_image) {
       fs.unlinkSync(path.join(process.cwd(), "public", upcoming.cover_image));
     }
-    res.status(httpStatus.BAD_REQUEST).json({
-      message: "This upcoming can only be remove by the person who created it",
-    });
-    return;
-  }
+    await upcomingModel.findByIdAndDelete(id);
 
-  if (upcoming.cover_image) {
-    fs.unlinkSync(path.join(process.cwd(), "public", upcoming.cover_image));
+    res.json({ message: "Deleted upcoming successfully" });
+  } catch (error) {
+    next(error);
   }
-  await upcomingModel.findByIdAndDelete(id);
-
-  res.json({ message: "Deleted upcoming successfully" });
 };
-export let update = async (req: express.Request, res: express.Response) => {
-  const { id } = req.params;
-  const { title, artist, genre, description, release_date } =
-    req.body as upcomingBody;
+export let update = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { id } = req.params;
+    const body = req.body as upcomingBody;
+    const { user } = req as any;
+    const cover = req.file?.filename;
 
-  const { user } = req as any;
-  const cover = req.file?.filename;
-  if (!isValidObjectId(id)) {
-    if (cover) {
-      fs.unlinkSync(
-        path.join(process.cwd(), "public", "upcomingCovers", cover)
+    if (!isValidObjectId(id)) {
+      throw httpErrors.BadRequest("upcoming id is not from mongodb");
+    }
+
+    const upcoming = await upcomingModel.findById(id);
+
+    if (!upcoming) {
+      throw httpErrors.NotFound("upcoming not found");
+    }
+
+    if (user._id !== upcoming.createBy && !user.isSuperAdmin) {
+      throw httpErrors.Forbidden(
+        "This upcoming can only be modified by the person who created it"
       );
     }
-    res
-      .status(httpStatus.BAD_REQUEST)
-      .json({ message: "upcoming id is not from mongodb" });
-    return;
-  }
 
-  const upcoming = await upcomingModel.findById(id);
-
-  if (!upcoming) {
-    if (cover) {
-      fs.unlinkSync(
-        path.join(process.cwd(), "public", "upcomingCovers", cover)
-      );
-    }
-    res.status(httpStatus.BAD_REQUEST).json({ message: "Upcoming not found" });
-    return;
-  }
-
-  if (user._id !== upcoming.createBy && !user.isSuperAdmin) {
-    if (req.file) {
-      fs.unlinkSync(path.join(process.cwd(), "public", req.file.filename));
-    }
-    res.status(httpStatus.BAD_REQUEST).json({
-      message:
-        "This upcoming can only be modified by the person who created it",
-    });
-    return;
-  }
-  if (cover) {
-    if (upcoming.cover_image)
+    if (cover && upcoming.cover_image) {
       fs.unlinkSync(path.join(process.cwd(), "public", upcoming.cover_image));
-  }
-  await upcomingModel.findByIdAndUpdate(id, {
-    title,
-    artist,
-    genre,
-    description,
-    release_date,
-    cover_image: cover && `/upcomingCovers/${cover}`,
-  });
+    }
+    await upcomingModel.findByIdAndUpdate(id, {
+      ...body,
+      cover_image: cover && `/upcomingCovers/${cover}`,
+    });
 
-  res.json({ message: "Updated upcoming successfully" });
+    res.json({ message: "Updated upcoming successfully" });
+  } catch (error) {
+    next(error);
+  }
 };
-export let getOne = async (req: express.Request, res: express.Response) => {
-  const { id } = req.params;
+export let getOne = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { id } = req.params;
 
-  if (!isValidObjectId(id)) {
-    res
-      .status(httpStatus.BAD_REQUEST)
-      .json({ message: "upcoming id is not from mongodb" });
-    return;
+    if (!isValidObjectId(id)) {
+      throw httpErrors.BadRequest("upcoming id is not from mongodb");
+    }
+
+    const upcoming = await upcomingModel
+      .findById(id)
+      .populate("artist", "fullName photo")
+      .populate("genre", "title description")
+      .populate("createBy", "name username profile")
+      .lean();
+
+    if (!upcoming) {
+      httpErrors.NotFound("upcoming not found");
+    }
+
+    res.json(upcoming);
+  } catch (error) {
+    next(error);
   }
-
-  const upcoming = await upcomingModel
-    .findById(id)
-    .populate("artist", "fullName photo")
-    .populate("genre", "title description")
-    .populate("createBy", "name username profile")
-    .lean();
-
-  if (!upcoming) {
-    res.status(httpStatus.BAD_REQUEST).json({ message: "Upcoming not found" });
-    return;
-  }
-
-  res.json(upcoming);
 };
-export let search = async (req: express.Request, res: express.Response) => {
-  const { upcoming } = req.query;
-  if (!upcoming) {
-    res
-      .status(httpStatus.BAD_REQUEST)
-      .json({ message: "upcoming title is required" });
-    return;
+export let search = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { upcoming } = req.query;
+    if (!upcoming) {
+      throw httpErrors.BadRequest("upcoming title is required");
+    }
+
+    const upcomingResult = await upcomingModel.find({
+      title: { $regex: upcoming },
+    });
+
+    res.json(upcomingResult);
+  } catch (error) {
+    next(error);
   }
-
-  const upcomingResult = await upcomingModel.find({
-    title: { $regex: upcoming },
-  });
-
-  res.json(upcomingResult)
-
 };

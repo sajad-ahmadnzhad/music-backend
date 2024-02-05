c434b955c471d9e4d719619664fbb3368a5f8ad6
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-02-05 22:23:35 +0330

    refactor: all apis user favorite

--- controllers/userFavorite.ts
+++ controllers/userFavorite.ts
@@ -1,100 +1,108 @@
-import express from "express";
+import { Request, Response, NextFunction } from "express";
 import { UserFavoriteBody } from "../interfaces/userFavorite";
 import userFavoriteModel from "../models/userFavorite";
 import albumModel from "../models/album";
 import musicModel from "../models/music";
 import httpStatus from "http-status";
+import httpErrors from "http-errors";
 import { isValidObjectId } from "mongoose";
-export let create = async (req: express.Request, res: express.Response) => {
-  const { type, target_id } = req.body as UserFavoriteBody;
-  const { user } = req as any;
+export let create = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { type, target_id } = req.body as UserFavoriteBody;
+    const { user } = req as any;
 
-  const album = await albumModel.findById(target_id);
-  const music = await musicModel.findById(target_id);
+    const album = await albumModel.findById(target_id);
+    const music = await musicModel.findById(target_id);
 
-  if (type === "music" && !music) {
-    res.status(httpStatus.NOT_FOUND).json({ message: "Music not found" });
-    return;
-  }
+    if (type === "music" && !music) {
+      throw httpErrors.NotFound("Music not found");
+    }
 
-  if (type === "albums" && !album) {
-    res.status(httpStatus.NOT_FOUND).json({ message: "Album not found" });
-    return;
-  }
+    if (type === "albums" && !album) {
+      throw httpErrors.NotFound("Album not found");
+    }
 
-  const userFavorite = await userFavoriteModel.findOne({
-    user: user._id,
-    target_id,
-  });
-
-  if (userFavorite) {
-    res.status(httpStatus.BAD_REQUEST).json({
-      message: "This album or music is already in the favorite list",
+    const userFavorite = await userFavoriteModel.findOne({
+      user: user._id,
+      target_id,
     });
-    return;
-  }
 
-  await userFavoriteModel.create({ type, target_id, user: user._id });
+    if (userFavorite) {
+      throw httpErrors.Conflict(
+        "This album or music is already in the favorite list"
+      );
+    }
 
-  res
-    .status(httpStatus.CREATED)
-    .json({ message: "Create user favorite successfully" });
-};
-export let getAll = async (req: express.Request, res: express.Response) => {
-  const { user } = req as any;
-  const favoriteList = await userFavoriteModel
-    .find({ user: user._id })
-    .populate("target_id", "title photo cover_image download_link")
-    .select("-user -__v")
-    .lean();
-  res.json(favoriteList);
-};
-export let remove = async (req: express.Request, res: express.Response) => {
-  const { id } = req.params;
-  const { user } = req as any;
+    await userFavoriteModel.create({ type, target_id, user: user._id });
 
-  if (!isValidObjectId(id)) {
     res
-      .status(httpStatus.NOT_FOUND)
-      .json({ message: "Favorite id is not found mongodb" });
-    return;
+      .status(httpStatus.CREATED)
+      .json({ message: "Create user favorite successfully" });
+  } catch (error) {
+    next(error);
   }
-
-  const favoriteList = await userFavoriteModel.findOneAndDelete({
-    user: user._id,
-    _id: id,
-  });
-
-  if (!favoriteList) {
-    res.status(httpStatus.NOT_FOUND).json({ message: "Favorite not found" });
-    return;
-  }
-  res.json({ message: "Deleted favorite successfully" });
 };
-export let getOne = async (req: express.Request, res: express.Response) => {
-  const { user } = req as any;
-  const { id } = req.params;
+export let getAll = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { user } = req as any;
+    const favoriteList = await userFavoriteModel
+      .find({ user: user._id })
+      .populate("target_id", "title photo cover_image download_link")
+      .select("-user -__v")
+      .lean();
 
-  if (!isValidObjectId(id)) {
-    res
-      .status(httpStatus.NOT_FOUND)
-      .json({ message: "Favorite id is not found mongodb" });
-    return;
+    res.json(favoriteList);
+  } catch (error) {
+    next(error);
   }
+};
+export let remove = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { id } = req.params;
+    const { user } = req as any;
 
-  const favorite = await userFavoriteModel
-    .findOne({
+    if (!isValidObjectId(id)) {
+      throw httpErrors.BadRequest("This favorite id is not from mongodb");
+    }
+
+    const favoriteList = await userFavoriteModel.findOneAndDelete({
       user: user._id,
       _id: id,
-    })
-    .populate("target_id", "title photo cover_image download_link")
-    .select("-user -__v")
-    .lean();
+    });
 
-  if (!favorite) {
-    res.status(httpStatus.NOT_FOUND).json({ message: "Favorite not found" });
-    return;
+    if (!favoriteList) {
+      throw httpErrors.NotFound("Favorite not found");
+    }
+
+    res.json({ message: "Deleted favorite successfully" });
+  } catch (error) {
+    next(error);
   }
+};
+export let getOne = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { user } = req as any;
+    const { id } = req.params;
 
-  res.json(favorite);
+    if (!isValidObjectId(id)) {
+      throw httpErrors.BadRequest("This favorite id is not from mongodb");
+    }
+
+    const favorite = await userFavoriteModel
+      .findOne({
+        user: user._id,
+        _id: id,
+      })
+      .populate("target_id", "title photo cover_image download_link")
+      .select("-user -__v")
+      .lean();
+
+    if (!favorite) {
+      throw httpErrors.NotFound("Favorite not found");
+    }
+
+    res.json(favorite);
+  } catch (error) {
+    next(error);
+  }
 };

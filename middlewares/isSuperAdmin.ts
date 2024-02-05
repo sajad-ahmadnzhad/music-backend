200519d4cfa0a34c1ab80b3ea59a0ac1132805ad
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-02-05 21:51:19 +0330

    refactor: isSupperAdmin middleware

--- middlewares/isSuperAdmin.ts
+++ middlewares/isSuperAdmin.ts
@@ -1,19 +1,20 @@
 import express from "express";
-import usersModel from "../models/users";
-import httpStatus from "http-status";
+import httpErrors from "http-errors";
+
 export default async (
   req: express.Request,
   res: express.Response,
   next: express.NextFunction
 ) => {
-  const { isSuperAdmin } = (req as any).user;
-  if (!isSuperAdmin) {
-    res
-      .status(httpStatus.BAD_REQUEST)
-      .json({
-        message: "This path is only active for the owner or super admin",
-      });
-    return;
+  try {
+    const { isSuperAdmin } = (req as any).user;
+    if (!isSuperAdmin) {
+      throw httpErrors.Forbidden(
+        "This path is only active for the owner or super admin"
+      );
+    }
+    next();
+  } catch (error) {
+    next(error);
   }
-  next();
 };

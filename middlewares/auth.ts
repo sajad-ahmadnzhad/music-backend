b768ec9af6997883f5df51f81645735c9463d895
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-02-05 21:44:08 +0330

    refactor: auth middleware

--- middlewares/auth.ts
+++ middlewares/auth.ts
@@ -1,43 +1,36 @@
-import express from "express";
-import httpStatus from "http-status";
+import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import httpErrors from "http-errors";
 import usersModel from "../models/users";
 import banUserModel from "../models/banUser";
 dotenv.config();
-export default async (
-  req: express.Request,
-  res: express.Response,
-  next: express.NextFunction
-) => {
-  const token = req.cookies?.token;
-  if (!token) {
-    res.status(httpStatus.UNAUTHORIZED).json({
-      message: "This path is protected. To access it, you must log in first",
-    });
-    return;
-  }
-  const checkToken = jwt.verify(token, process.env.JWT_SECRET as string);
-  if (checkToken) {
+export default async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      throw httpErrors.Forbidden(
+        "This path is protected. To access it, you must log in first"
+      );
+    }
+    const checkToken = jwt.verify(token, process.env.JWT_SECRET as string);
+
     const user = await usersModel
       .findById((checkToken as any).id)
       .select("-password");
 
     if (!user) {
-      res.status(httpStatus.NOT_FOUND).json({ message: "User not found" });
-      return;
+      throw httpErrors.NotFound("User not found");
     }
+    
     const isBanUser = await banUserModel.findOne({ email: user.email });
     if (isBanUser) {
-      res
-        .status(httpStatus.BAD_REQUEST)
-        .json({ message: "You have been banned by admin" });
-      return;
+      throw httpErrors.Forbidden("You have been banned by admin");
     }
 
     (req as any).user = user;
     next();
-    return;
+  } catch (error) {
+    next(error);
   }
-  res.status(httpStatus.BAD_REQUEST).json({ message: "Token is not valid" });
 };

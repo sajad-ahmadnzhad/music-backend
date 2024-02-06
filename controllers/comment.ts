52e0d87a1b9bc13335145e091f708c8c63b96801
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-02-07 00:51:42 +0330

    edit: api create comment

--- controllers/comment.ts
+++ controllers/comment.ts
@@ -2,11 +2,24 @@ import { Request, Response, NextFunction } from "express";
 import { CommentsBody } from "../interfaces/comment";
 import commentModel from "../models/comment";
 import httpStatus from "http-status";
+import { isValidObjectId } from "mongoose";
+import httpErrors from "http-errors";
+import musicModel from '../models/music'
 export let create = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const body = req.body as CommentsBody;
     const { user } = req as any;
     const { musicId } = req.params;
+
+    if (!isValidObjectId(musicId)) {
+      throw httpErrors.BadRequest("This music id is not from mongodb");
+    }
+
+    const music = await musicModel.findById(musicId).lean();
+
+    if (!music) {
+      throw httpErrors.NotFound("Music not found");
+    }
 
     await commentModel.create({ ...body, creator: user._id, music: musicId });
 

97e3af624625b4f60c7e05c754adc59e2c08eadf
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-02-07 16:44:11 +0330

    feat: Change the structure of the comments validator

--- validators/comment.ts
+++ validators/comment.ts
@@ -1,30 +1,5 @@
 import joi from "joi";
-import commentModel from "../models/comment";
-import musicModel from "../models/music";
 export default joi.object({
-  body: joi.string().min(3).max(2000).required(),
-  mainCommentID: joi
-    .string()
-    .regex(/^[0-9a-fA-F]{24}$/)
-    .message("Comment id is not from mongodb")
-    .optional()
-    .external(async (value, helpers) => {
-      if (value) {
-        const comment = await commentModel.findOne({ _id: value });
-        if (!comment) return helpers.error("Comment not found");
-      }
-    }),
-
-  music: joi
-    .string()
-    .regex(/^[0-9a-fA-F]{24}$/)
-    .message("Music id is not from mongodb")
-    .optional()
-    .external(async (value, helpers) => {
-      if (value) {
-        const music = await musicModel.findOne({ _id: value });
-        if (!music) return helpers.error("Music not found");
-      }
-    }),
+  comment: joi.string().min(3).max(2000).required(),
   score: joi.number().valid(1, 2, 3, 4, 5),
 });

8f5bb5e1ca4f64d9f10368beb8e4a9ae131302a2
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-02-15 14:11:03 +0330

    feature: Avoid sending invalid usernames

--- validators/register.ts
+++ validators/register.ts
@@ -1,7 +1,13 @@
 import Joi from "joi";
 export default Joi.object({
   name: Joi.string().trim().min(3).max(15).required(),
-  username: Joi.string().trim().min(5).max(20).required(),
+  username: Joi.string()
+    .regex(/^[a-zA-Z0-9_]+$/)
+    .message("Username is not valid")
+    .trim()
+    .min(5)
+    .max(20)
+    .required(),
   email: Joi.string()
     .trim()
     .regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)

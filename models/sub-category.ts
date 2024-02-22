a7004d884b0961fbc1c8a7f5824c054746d323c4
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-02-22 21:37:04 +0330

    feat: create sub category file and add feature to category

--- models/sub-category.ts
+++ models/sub-category.ts
@@ -2,11 +2,9 @@ import { Schema, model } from "mongoose";
 const schema = new Schema(
   {
     title: { type: String, required: true },
-    image: { type: String },
-    sub_category: [{ type: Schema.ObjectId, ref: "subCategory", required: true }],
     description: { type: String },
   },
   { timestamps: true }
 );
 
-export default model("category", schema);
+export default model("subCategory", schema);

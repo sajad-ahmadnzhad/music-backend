28d04eb87ea06fa242d734c2d0ca7ced9149ac2e
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-03-06 16:52:35 +0330

    feature: added advanced populate and pagination in api get all auto archive

--- controllers/autoArchive.ts
+++ controllers/autoArchive.ts
@@ -1,14 +1,46 @@
 import { Request, Response, NextFunction } from "express";
+import pagination from "../helpers/pagination";
 import autoArchiveModel from "../models/autoArchive";
+
 export let getAll = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const autoArchives = await autoArchiveModel
+    const query = autoArchiveModel
       .find()
-      .populate("target_ids")
+      .populate({
+        path: "target_ids",
+        select:
+          "title description artist genre cover_image fullName englishName nickname photo musicStyle download_link",
+        populate: [
+          {
+            path: "artist",
+            select: "fullName englishName photo musicStyle",
+            strictPopulate: false,
+            populate: [
+              {
+                path: "musicStyle",
+                select: "title description",
+                strictPopulate: false,
+              },
+            ],
+          },
+          {
+            path: "musicStyle",
+            select: "title description",
+            strictPopulate: false,
+          },
+          { path: "genre", select: "title description", strictPopulate: false },
+        ],
+      })
       .populate("country", "title description image")
+      .select("-__v")
       .sort({ createdAt: -1 })
       .lean();
-    res.json(autoArchives);
+
+    const data = await pagination(req, query, autoArchiveModel);
+
+    if (data.error) throw data.error;
+
+    res.json(data);
   } catch (error) {
     next(error);
   }

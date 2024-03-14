b6e67829874d5ab210485cdfd961b3d36f9c3134
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-03-14 14:59:02 +0330

    feat: create model notificattion

---
+++ models/notification.ts
@@ -0,0 +1,44 @@
+import { Schema, model } from "mongoose";
+
+const schema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+    creator: {
+      type: Schema.ObjectId,
+      ref: "users",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: [
+        "category",
+        "music",
+        "playList",
+        "album",
+        "users",
+        "upcoming",
+        "other",
+      ],
+      default: "other",
+    },
+    receiver: {
+      type: Schema.ObjectId,
+      ref: "users",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default model("notification", schema);

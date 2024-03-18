c8545ba4c95e1b4a15798199c97509d3c93684ed
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-03-18 22:35:29 +0330

    feat: create api get read and get unread

--- controllers/serverNotification.ts
+++ controllers/serverNotification.ts
@@ -12,6 +12,7 @@ export let getAll = async (req: Request, res: Response, next: NextFunction) => {
       })
       .populate("target_id", "title description image")
       .select("-__v -receiver")
+      .sort({ createdAt: -1 })
       .lean();
 
     res.json(severNotifications);
@@ -45,6 +46,75 @@ export let read = async (req: Request, res: Response, next: NextFunction) => {
     });
 
     res.json({ message: "The sever notification has been read successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+export let readAll = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { user } = req as any;
+    const existingServerNotificationUnRead =
+      await serverNotificationModel.findOne({
+        receiver: user._id,
+        isRead: false,
+      });
+
+    if (!existingServerNotificationUnRead) {
+      throw httpErrors.NotFound("No unread server notification found");
+    }
+
+    await serverNotificationModel.updateMany(
+      { receiver: user._id },
+      {
+        isRead: true,
+      }
+    );
+
+    res.json({
+      message: "All sever notifications have been successfully read",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+export let getUnread = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { user } = req as any;
+    const serverNotifications = await serverNotificationModel
+      .find({ isRead: false, receiver: user._id })
+      .populate("target_id", "title description image")
+      .select("-__v -receiver")
+      .sort({ createdAt: -1 })
+      .lean();
+
+    res.json(serverNotifications);
+  } catch (error) {
+    next(error);
+  }
+};
+export let getRead = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { user } = req as any;
+    const notifications = await serverNotificationModel
+      .find({ isRead: true, receiver: user._id })
+      .populate("target_id", "title description image")
+      .select("-__v -receiver")
+      .sort({ createdAt: -1 })
+      .lean();
+
+    res.json(notifications);
   } catch (error) {
     next(error);
   }
--- routes/serverNotification.ts
+++ routes/serverNotification.ts
@@ -1,11 +1,20 @@
 import express from "express";
-import { getAll, read } from "../controllers/serverNotification";
+import {
+  getAll,
+  getRead,
+  getUnread,
+  read,
+  readAll,
+} from "../controllers/serverNotification";
 import authMiddlewares from "../middlewares/auth";
 import isBanMiddlewares from "../middlewares/isBan";
 const router = express.Router();
 router.use(authMiddlewares, isBanMiddlewares);
 router.get("/", getAll);
 
+router.put("/read-all", readAll);
+router.get("/read", getRead);
+router.get("/unread", getUnread);
 router.put("/read/:id", read);
 
 export default router;

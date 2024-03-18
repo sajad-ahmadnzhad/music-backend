ebdd059462ee1c8979e5e64364c4862634fabee0
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-03-18 22:05:25 +0330

    feat: create api read server notification

--- controllers/serverNotification.ts
+++ controllers/serverNotification.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import serverNotificationModel from "../models/serverNotification";
+import { isValidObjectId } from "mongoose";
+import httpErrors from "http-errors";
 
 export let getAll = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -10,9 +12,39 @@ export let getAll = async (req: Request, res: Response, next: NextFunction) => {
       })
       .populate("target_id", "title description image")
       .select("-__v -receiver")
-      .lean()
+      .lean();
 
     res.json(severNotifications);
+  } catch (error) {
+    next(error);
+  }
+};
+export let read = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { user } = req as any;
+    const { id } = req.params;
+
+    if (!isValidObjectId(id)) {
+      throw httpErrors.BadRequest(
+        "This server notification id is not from mongodb"
+      );
+    }
+
+    const existingServerNotification = await serverNotificationModel.findOne({
+      _id: id,
+      isRead: false,
+      receiver: user._id,
+    });
+
+    if (!existingServerNotification) {
+      throw httpErrors.NotFound("Sever notification id is not from mongodb");
+    }
+
+    await existingServerNotification.updateOne({
+      isRead: true,
+    });
+
+    res.json({ message: "The sever notification has been read successfully" });
   } catch (error) {
     next(error);
   }
--- routes/serverNotification.ts
+++ routes/serverNotification.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { getAll } from "../controllers/serverNotification";
-import authMiddlewares from '../middlewares/auth'
-import isBanMiddlewares from '../middlewares/isBan'
+import { getAll, read } from "../controllers/serverNotification";
+import authMiddlewares from "../middlewares/auth";
+import isBanMiddlewares from "../middlewares/isBan";
 const router = express.Router();
-router.use(authMiddlewares , isBanMiddlewares)
+router.use(authMiddlewares, isBanMiddlewares);
 router.get("/", getAll);
+
+router.put("/read/:id", read);
 
 export default router;
